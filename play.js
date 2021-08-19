d96cfb27daa63de41dc60c239fb398238786ba1a
Gareth <gar.carey@gmail.com>
2021-08-18 20:45:40 -0400

    refactor of file after moving functions to new file

--- play.js
+++ play.js
@@ -1,18 +1,6 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
-
-const handleUserInput = (input) => {
-  if (input === '\u0003'){
-    process.exit();
-  }
-};
+setupInput();
