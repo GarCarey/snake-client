1d69780bbae2307d63463c53b977113dafb3e584
Gareth <gar.carey@gmail.com>
2021-08-18 21:15:40 -0400

    add input functionality to accept commands based on which direction we want to move

--- input.js
+++ input.js
@@ -1,7 +1,11 @@
-const setupInput = () => {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
+  stdin.on('data', handleUserInput);
   stdin.resume();
   return stdin;
 }
@@ -9,6 +13,15 @@ const setupInput = () => {
 const handleUserInput = (input) => {
   if (input === '\u0003'){
     process.exit();
+  }
+  if (input === 'w'){
+    connection.write('Move: up');
+  } else if (input === 's') {
+    connection.write('Move: down');
+  } else if (input === 'a') {
+    connection.write('Move: left');
+  } else if (input === 'd') {
+    connection.write('Move: right');
   }
 };
 
