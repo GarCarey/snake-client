8b7043061f3d83544b8ded6bd74683563ef0b9e1
Gareth <gar.carey@gmail.com>
2021-08-19 20:44:13 -0400

    add comments file

--- constants.js
+++ constants.js
@@ -1,6 +1,7 @@
 const IP = '192.168.2.168';
 const PORT =  50541;
 
+//object of inputs to control the snake
 const moves = {
   w: 'Move: up',
   a: "Move: left",
@@ -8,11 +9,13 @@ const moves = {
   d: "Move: right"
 };
 
+//object of inputs that will be printed
 const outputs = {
   z: 'Keep going',
   x: 'New High Score!'
 }
 
+//values and objects being exported to input and client files
 module.exports = {
   IP,
   PORT,
