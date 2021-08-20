af290370b08f60411b6f832fc2c05eed4dd87c1b
Gareth <gar.carey@gmail.com>
2021-08-19 20:42:15 -0400

    add comments to file

--- play.js
+++ play.js
@@ -1,5 +1,7 @@
+//Modules being received from input and client files
 const { connect } = require('./client');
 const { setupInput } = require('./input');
 
+//printed when we begin connecting to game server
 console.log('Connecting ...');
 setupInput(connect());
