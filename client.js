d59310ba0dc0b13ab19060c267bd3209c7605764
Gareth <gar.carey@gmail.com>
2021-08-19 20:44:33 -0400

    add comments to file

--- client.js
+++ client.js
@@ -1,20 +1,25 @@
 const net = require('net');
 const { IP, PORT } = require('./constants');
 
+//function that creates a connection to the game server
 const connect = () => {
   const conn = net.createConnection({
     host: IP,
     port: PORT
   });
 
+  //interpret incoming data as text
   conn.setEncoding('utf8');
 
+  //function that prints we have connected to the game in the server and writes our
+  // initals above the snake when we connect
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write("Name: GLC");
-    //conn.write("Move: up");
   });
 
+  //prints out data return from the game server after specific actions 
+  // IE dying in the game
   conn.addListener('data', data => {
     console.log('Server say: ', data);
   });
@@ -22,4 +27,5 @@ const connect = () => {
   return conn;
 }
 
+//function being export to Play file
 module.exports = { connect };
